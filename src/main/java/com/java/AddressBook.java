3403530af8e50d746446dba4b71f402ac3cb2c30
Ranidhumma <ranidhumma8@gmail.com>
2022-03-27 16:18:48 +0530

    Edit contacts by using contact name

--- src/main/java/com/java/AddressBook.java
+++ src/main/java/com/java/AddressBook.java
@@ -2,23 +2,45 @@ package com.java;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBook {
 	public static void main(String[] args) {
-
 		System.out.println("**Welcome to Address Book**");
+		Scanner sc = new Scanner(System.in);
 
 		/* Arraylist is used to store data/contacts */
-
-		List<ContactPerson> contacts = new ArrayList<>();
+		List<ContactPerson> contacts = new ArrayList<ContactPerson>();
 		AddressBookNew ab = new AddressBookNew();
-		ContactPerson newContact = new ContactPerson("Rani", "Dhumma", "xyz", "Solapur", "Maharashtra", 410004,
-				994445555, "@gmail.com");
+		// ab.addContact();
 
-		contacts.add(newContact);
+		while (true) {
 
-		ab.addContact(contacts);
-		System.out.println(contacts);
+			System.out.println("Enter your Choice: \n 1.Add Contact\n 2. Edit Contact\n 3.Display Contact\n 4.Exit ");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Enter the contact detais to add in the Address Book: ");
+				ab.addContact();
 
+				break;
+			case 2:
+				System.out.println("Enter the detais to edit: ");
+				ab.editContact();
+
+				break;
+
+			case 3:
+				System.out.println("The contacts in the Address Book are ");
+				ab.displayContact();
+				break;
+
+			case 4:
+				System.out.println("Exit : ");
+				sc.close();
+				return;
+
+			}
+		}
 	}
 }
